ffec876d33aa1e964ced674df25061950e5ff95e
jakebillings <jake@eakjb.com>
2015-07-05 13:53:35 -0600

    Added setup and README

---
+++ README.md
@@ -0,0 +1,6 @@
+CordovaHelloWorld
+=================
+
+CordovaHelloWorld is a template for cross-platform development using Yeoman webapp, Cordova, and Materialize.
+
+
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+textReset=$(tput sgr0)
+textGreen=$(tput setaf 2)
+message_info () {
+  echo "${textGreen}[my-app]${textReset} $1"
+}
+
+message_info "Running npm install..."
+npm install
+
+message_info "Running bower install..."
+bower install
+
+message_info "Adding platforms..."
+cordova platform add ios
+cordova platform add android
+cordova platform add amazon-fireos
+
+message_info "Installing plugins..."
+cordova plugin add org.apache.cordova.statusbar
+
+message_info "Running a grunt build..."
+grunt build
